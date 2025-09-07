07b0a0d6041719349222bbc8079b92e4f2d3c14d
Bob Cheg <89573050+BobCheg@users.noreply.github.com>
2025-09-07 23:40:04 +0530

    Add project files.

---
+++ Hospitals.Utilities/DbInitializer.cs
@@ -0,0 +1,62 @@
+﻿using Hospital.Models;
+using Hospital.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospitals.Utilities
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private ApplicationDbContext _context;
+
+        public DbInitializer(
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager, 
+            ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if(_context.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _context.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            if (!_roleManager.RoleExistsAsync(WebSiteRoles.WebSite_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Patient)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.WebSite_Doctor)).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "Jambesh",
+                    Email = "jambesh@xyz.com"
+                }, "Jamesh@123").GetAwaiter().GetResult();
+            }
+
+            var AppUser = _context.ApplicationUsers.FirstOrDefault(x => x.Email == "jambesh@xyz.com");
+
+            if(AppUser != null)
+            {
+                _userManager.AddToRoleAsync(AppUser, WebSiteRoles.WebSite_Admin).GetAwaiter().GetResult();
+            }
+        }
+
+       
+
+     
+    }
+}
